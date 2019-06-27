0fb96cf3169b0878a3809ece163314e6bfcaa34f
johannagoenagac <johannagoenagac@gmail.com>
2019-06-27 13:21:24 -0300

    se agrega nueva seccion de proyectos, sobre mi

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,4 @@
 window.onload =() =>{
 
-  document.getElementById("more").addEventListener("click",()=>{
-
-    document.getElementById("about-me").style.display="block";
-    document.getElementById("coverpage").style.display="none";
-   
-  })
   
-
-
-
-  }
-
-
-
+}
